a9352d0ea1197cb1d81ded1dc47ba1f9ab4e6c17
Joey Piccola <joey@joeypiccola.com>
2016-11-08 15:16:42 -0700

    hmm

---
+++ spec/classes/timezone_win_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'timezone_win' do
+  let(:facts) {{
+  }}
+  let(:params) {{
+    :timezone => 'Mountain Standard Time',
+  }}
+  it { should compile }
+  
+  it { should contain_exec('set-timezone').with(
+    'command'  => "tzutil /s 'Mountain Standard Time'",
+    'onlyif'   => "if ((Get-WmiObject -Class win32_timezone).StandardName -eq 'Mountain Standard Time') {exit 1}",
+    'provider' => "powershell"
+    )
+  }
+  
+end
