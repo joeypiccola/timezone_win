ec742eea315476a5ac663aef4a1cc341acef6f69
Joey Piccola <joey@joeypiccola.com>
2016-11-08 14:48:17 -0700

    added tests docs

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
-describe 'timezone_win' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('timezone_win') }
-  end
+describe 'timezone_win' do
+  let(:facts) {{
+  }}
+  let(:params) {{
+    :timezone => 'Mountain Standard Time',
+  }}
+  it { should compile }
+  
+  it { should contain_exec('set-timezone').with(
+    'command'  => "tzutil /s 'Mountain Standard Time'",
+    'onlyif'   => "if ((Get-WmiObject -Class win32_timezone).StandardName -eq 'Mountain Standard Time') {exit 1}",
+    'provider' => "powershell"
+    )
+  }
+  
 end
