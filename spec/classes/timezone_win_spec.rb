87f7efbf9a8745cdb60ab8688c7d62bfa20d2814
Joey Piccola <joey@joeypiccola.com>
2016-11-09 15:22:37 -0700

    fixed spec arg

--- spec/classes/timezone_win_spec.rb
+++ spec/classes/timezone_win_spec.rb
@@ -1,17 +1,18 @@
 # for rspec-puppet documentation - see http://rspec-puppet.com/tutorial/
 require_relative '../spec_helper'
-describe 'davita_timezone' do
+describe 'timezone_win' do
   let(:facts) {{
     :concat_basedir         => '/var/tmp',
   }}
   let(:params) {{
-    :timezone => 'blah',
+    :timezone => 'Mountain Standard Time',
   }}
   it { should compile }
   
   it { should contain_exec('set-timezone').with(
-    'command' => "tzutil /s 'blah'",
-    'onlyif'  => "if ((Get-WmiObject -Class win32_timezone).StandardName -eq 'blah') {exit 1}",
+    'command'  => "tzutil /s 'Mountain Standard Time'",
+    'onlyif'   => "if ((Get-WmiObject -Class win32_timezone).StandardName -eq 'Mountain Standard Time') {exit 1}",
+	'provider' => 'powershell'
     )
   }
   
